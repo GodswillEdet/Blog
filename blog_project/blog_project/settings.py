1dbfd32f25326e86a74b5327e5a362d072b84496
GodswillEdet <gemzeeedet@gmail.com>
2022-08-17 11:57:32 -0700

    forms and users account

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
     #local
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +116,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
